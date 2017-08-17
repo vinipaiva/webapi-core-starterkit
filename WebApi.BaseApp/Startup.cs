d5248d9a4e3a2c048eca7ae6f3be33dc6b893c18
DATACOM-NZ\ViniciusP <viniciusp@datacom.co.nz>
2017-08-17 15:47:25 +1200

    Added the real initial structure with swagger setup

---
+++ WebApi.BaseApp/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
+
+namespace WebApi.BaseApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "My Core.WebApi Starter kit",
+                    Version = "v1",
+                    Description = "This is a WebApi.Core starter kit.",
+                    //TermsOfService = "None",
+                    //Contact = new Contact { Name = "Vinicius Paiva", Email = "vinipaiva@gmail.com", Url = "https://www.linkedin.com/in/vinicius-da-silva-50963923" },
+                });
+
+                //Set the comments path for the swagger json and ui.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "WebApi.BaseApp.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+            services.AddMvcCore().AddApiExplorer();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Swagger config
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Core.WebApi.StarterKit V1");
+            });
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
